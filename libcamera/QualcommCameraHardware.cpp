c227a4925aeb53ba12bdd927223607f88ee7c26c
Sean Neeley <sean.neeley@gmail.com>
2011-04-21 18:09:51 -0400

    zeppelin: libcamera: corrections for LED_MODE_ON mode
    Work around for problem where when LED_MODE_ON is used, flash turns off after focusing, when it should remain on.
    
    Change-Id: I4f4bf299ff7d717837be2292b008942c43dba42b

--- libcamera/QualcommCameraHardware.cpp
+++ libcamera/QualcommCameraHardware.cpp
@@ -1477,6 +1477,7 @@ void QualcommCameraHardware::stopPreview()
 void QualcommCameraHardware::runAutoFocus()
 {
     int status = false;
+    led_mode_t value;
 
     mAutoFocusThreadLock.lock();
     mAutoFocusFd = open(MSM_CAMERA_CONTROL, O_RDWR);
@@ -1496,6 +1497,16 @@ void QualcommCameraHardware::runAutoFocus()
     native_set_afmode(camerafd, AF_MODE_MACRO);
     if (native_get_af_result(camerafd) == 0) {
         status = true;
+    }
+
+    // firesnatch 04/21/2011 - native_set_afmode turns off LED light 
+    // for some reason.  Need to toggle it to get it back on.
+    value = (led_mode_t) getParm("flash-mode", flashmode);
+    if (value == LED_MODE_ON) {
+        value = LED_MODE_OFF;
+        native_set_parm(CAMERA_SET_PARM_LED_MODE, sizeof(value), (void *)&value);
+        value = LED_MODE_ON;
+        native_set_parm(CAMERA_SET_PARM_LED_MODE, sizeof(value), (void *)&value);
     }
 
     mAutoFocusThreadRunning = false;
@@ -2406,5 +2417,6 @@ status_t QualcommCameraHardware::sendCommand(int32_t command, int32_t arg1,
 }
 
 }; // namespace android
+
 
 
